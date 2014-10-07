945b624779d3bd68e1fcdb686e9f8205b1f5801b
MYP <sword.arch@yahoo.co.id>
2014-10-08 00:33:21 +0700

    first commit

---
+++ myudipermana.java
@@ -0,0 +1,18 @@
+
+package id.blits.tugas;
+
+//PROGRAM INI DIBUAT OLEH SINDI OKVANIAR...
+public class SindiOkvaniar {
+
+    
+    public static void main(String[] args) {
+        String nama = "M Yudi Permana";
+        String npm = "13311342";
+        String kelas = "SI 13 E";
+        System.out.println("Perguruan Tinggi TEKNOKRAT");
+        System.out.println("============================");
+        System.out.println("Nama   : "+nama+"\n"+"NPM    : "+npm+"\n"+"Kelas  : "+kelas);
+     
+    }
+    
+}
