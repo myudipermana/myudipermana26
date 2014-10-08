30c06885c598c93c998d8f376e8c15b4cba1c6f0
MYP <sword.arch@yahoo.co.id>
2014-10-08 13:42:31 +0700

    second commit

---
+++ src/id/blits/myudipermana.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.blits;
+
+/**
+ *
+ * @author USER
+ */
+public class myudipermana {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nama = "M Yudi Permana";
+        String npm = "13311342";
+        String kelas = "SI 13 E";
+        System.out.println("Perguruan Tinggi TEKNOKRAT STMIK");
+        System.out.println("Nama   : "+nama+"\n"+"NPM    : "+npm+"\n"+"Kelas  : "+kelas);
+    }
+    
+}
